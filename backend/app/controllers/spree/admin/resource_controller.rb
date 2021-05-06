fdddbbe3edd8c4341f48123bc1ba89044ff45aca
Thomas von Deyen <thomas@vondeyen.com>
2021-05-06 15:22:37 +0200

    Use symbol for parent_model_name in resource controller
    Rails does not allow Strings in polymorphic urls anymore for security
    reasons.

--- backend/app/controllers/spree/admin/resource_controller.rb
+++ backend/app/controllers/spree/admin/resource_controller.rb
@@ -189,15 +189,12 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
   end
 
   def parent
-    if parent?
-      @parent ||= self.class.parent_data[:model_class]
-                    .includes(self.class.parent_data[:includes])
-                    .find_by(self.class.parent_data[:find_by] => params["#{parent_model_name}_id"])
-      instance_variable_set("@#{parent_model_name}", @parent)
-    else
-      Spree::Deprecation.warn "Calling #parent is deprecated on a ResourceController which has not defined a belongs_to"
-      nil
-    end
+    @parent ||= self.class.parent_data[:model_class]
+                  .includes(self.class.parent_data[:includes])
+                  .find_by!(self.class.parent_data[:find_by] => params["#{parent_model_name}_id"])
+    instance_variable_set("@#{parent_model_name}", @parent)
+  rescue ActiveRecord::RecordNotFound => e
+    resource_not_found(flash_class: e.model.constantize, redirect_url: spree.polymorphic_url([:admin, parent_model_name.pluralize.to_sym]))
   end
 
   def parent?
